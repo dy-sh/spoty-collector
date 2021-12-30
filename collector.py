cf1f3608cbac160440592c738f4c7596919a017e
Derwish <derwish.pro@gmail.com>
2021-12-30 04:26:45 +0300

    -

--- collector.py
+++ collector.py
@@ -22,8 +22,8 @@ Next, use "update" command to create mirrors and update it (see "update --help")
     """
     playlist_ids = spoty.utils.tuple_to_list(playlist_ids)
     new_subs = col.subscribe(playlist_ids, mirror_name)
-    mirrors=col.read_mirrors()
-    all_subs=col.get_subscriptions(mirrors)
+    mirrors = col.read_mirrors()
+    all_subs = col.get_subscriptions(mirrors)
     click.echo(f'{len(new_subs)} new playlists added to subscriptions (total subscriptions: {len(all_subs)}).')
 
 
@@ -39,9 +39,9 @@ Unsubscribe from the specified playlists (by playlist ID or URI).
     """
     playlist_ids = spoty.utils.tuple_to_list(playlist_ids)
     unsubscribed = col.unsubscribe(playlist_ids, remove_mirror, remove_tracks)
-    mirrors=col.read_mirrors()
-    all_subs=col.get_subscriptions(mirrors)
-    click.echo(f'Total {len(unsubscribed)} playlists unsubscribed (total subscriptions remain: {len(all_subs)}).')
+    mirrors = col.read_mirrors()
+    all_subs = col.get_subscriptions(mirrors)
+    click.echo(f'{len(unsubscribed)} playlists unsubscribed (subscriptions remain: {len(all_subs)}).')
 
 
 @collector.command("unsub-all")
@@ -51,7 +51,10 @@ def unsubscribe_all(remove_mirror):
     """
 Unsubscribe from all specified playlists.
     """
-    col.unsubscribe_all(remove_mirror)
+    unsubscribed = col.unsubscribe_all(remove_mirror)
+    mirrors = col.read_mirrors()
+    all_subs = col.get_subscriptions(mirrors)
+    click.echo(f'{len(unsubscribed)} playlists unsubscribed (subscriptions remain: {len(all_subs)}).')
 
 
 @collector.command("unsub-mirror")
@@ -64,7 +67,10 @@ Unsubscribe from playlists for which the specified mirror playlists has been cre
 Specify IDs or URIs of mirror playlists.
     """
     playlist_ids = spoty.utils.tuple_to_list(playlist_ids)
-    col.unsubscribe_mirror(playlist_ids, remove_mirror)
+    unsubscribed = col.unsubscribe_mirrors(playlist_ids, remove_mirror)
+    mirrors = col.read_mirrors()
+    all_subs = col.get_subscriptions(mirrors)
+    click.echo(f'{len(unsubscribed)} playlists unsubscribed (subscriptions remain: {len(all_subs)}).')
 
 
 @collector.command("list")
--- collector_plugin.py
+++ collector_plugin.py
@@ -37,6 +37,14 @@ def get_subscriptions(mirrors: dict):
     return all_subs
 
 
+def remove_mirror_if_empty(mirror):
+    pass
+
+
+def remove_tracks_from_mirror(mirror, playlist_id):
+    pass
+
+
 def subscribe(playlist_ids: list, mirror_name=None):
     mirrors = read_mirrors()
     subscribed = []
@@ -80,7 +88,7 @@ def unsubscribe(playlist_ids: list, remove_mirror=False, remove_tracks=False):
 
         playlist = spotify_api.get_playlist(playlist_id)
         if playlist is None:
-            click.echo(f'PLaylist "{playlist_id}" not found.')
+            click.echo(f'Playlist "{playlist_id}" not found.')
             continue
 
         all_subs = get_subscriptions(mirrors)
@@ -92,6 +100,10 @@ def unsubscribe(playlist_ids: list, remove_mirror=False, remove_tracks=False):
             if playlist_id in subs:
                 mirrors[mirror].remove(playlist_id)
                 unsubscribed.append(playlist_id)
+                if remove_mirror:
+                    remove_mirror_if_empty(mirror)
+                if remove_tracks:
+                    remove_tracks_from_mirror(mirror, playlist_id)
                 click.echo(f'Unsubscribed from playlist "{playlist["name"]}".')
 
     write_mirrors(mirrors)
@@ -100,15 +112,41 @@ def unsubscribe(playlist_ids: list, remove_mirror=False, remove_tracks=False):
 
 
 def unsubscribe_all(remove_mirror=False):
-    return None
+    mirrors = read_mirrors()
+    subs = get_subscriptions(mirrors)
+    unsubscribed = unsubscribe(subs, remove_mirror)
+    return unsubscribed
 
 
-def unsubscribe_mirror(playlist_ids: list, remove_mirror=False):
-    return None
+def unsubscribe_mirrors(playlist_ids: list, remove_mirror=False):
+    mirrors = read_mirrors()
+    all_unsubscribed = []
+    for playlist_id in playlist_ids:
+        mirror_playlist = spotify_api.get_playlist(playlist_id)
+        if mirror_playlist is None:
+            click.echo(f'Mirror playlist "{playlist_id}" not found.')
+            continue
+        mirror = mirror_playlist["name"]
+        if mirror not in mirrors:
+            click.echo(f'Playlist "{mirror}" ({playlist_id}) is not a mirror playlist.')
+            continue
+        unsubscribed = unsubscribe(mirrors[mirror], remove_mirror)
+        all_unsubscribed.extend(unsubscribed)
+    return all_unsubscribed
 
 
 def list():
-    return None
+    mirrors=read_mirrors()
+    all_playlists = get_subscriptions(mirrors)
+    for mirror, playlist_ids in mirrors.items():
+        click.echo(f'Mirror "{mirror}":')
+        for playlist_id in playlist_ids:
+            playlist = spotify_api.get_playlist(playlist_id)
+            if playlist is None:
+                click.echo(f'  Playlist "{playlist_id}" not found.')
+                continue
+            click.echo(f'  {playlist_id} "{playlist["name"]}"')
+    click.echo(f'Total {len(all_playlists)} playlists in {len(mirrors.items())} mirrors.')
 
 
 def update():
